c38ffeeef840a51602737a5bce0ed76a0243c7c8
Nguyễn Tiến Đạt <ngtiendat0411@gmail.com>
2022-10-17 11:30:21 +0700

    add

--- lib/login.dart
+++ lib/login.dart
@@ -1,31 +1,26 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/main.dart';
+import 'package:flutter_application_1/Signin.dart';
 
-void main() {
-  runApp(LoginScreen1());
-}
-
-class LoginScreen1 extends StatefulWidget {
-  const LoginScreen1({super.key});
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
   @override
-  State<LoginScreen1> createState() => _LoginScreenState();
+  State<LoginScreen> createState() => _LoginScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen1> {
+class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          body: Padding(
-        padding: const EdgeInsets.only(top: 15),
-        child: Container(
-            color: Colors.white,
-            child: Column(children: [
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
               Container(
                 width: 50,
                 height: 50,
-                padding: const EdgeInsets.all(15),
+                padding: const EdgeInsets.all(16),
                 decoration: const BoxDecoration(
                   shape: BoxShape.circle,
                   color: Colors.blue,
@@ -33,166 +28,69 @@ class _LoginScreenState extends State<LoginScreen1> {
                 child: const FlutterLogo(),
               ),
               const Padding(
-                padding: EdgeInsets.all(8.0),
+                padding: EdgeInsets.only( top :20),
                 child: Text(
-                  'Đăng ký ',
+                  'Bạn đồng hành của người yêu cây trồng ',
                   style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Color.fromARGB(255, 74, 64, 255),
-                    fontSize: 20,
+                    color: Colors.black,
+                    fontSize: 18,
                   ),
                 ),
               ),
-              const Text(
-                'Chỉ dành cho thuê bao Viettel',
-                style: TextStyle(color: Colors.black, fontSize: 17),
-              ),
-              const Padding(
-                padding: const EdgeInsets.only(left: 25, right: 25, top: 10),
-                child: TextField(
-                  style: TextStyle(fontSize: 20, color: Colors.black),
-                  decoration: InputDecoration(
-                      labelText: 'Số điện thoại',
-                      labelStyle: TextStyle(color: Colors.grey, fontSize: 15),
-                      border: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black),
-                          borderRadius: BorderRadius.all(Radius.circular(10)))),
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.only(left: 25, right: 25, top: 10),
-                child: const TextField(
-                  decoration: InputDecoration(
-                      labelText: 'Tạo mật khẩu',
-                      labelStyle: TextStyle(color: Colors.grey, fontSize: 15),
-                      border: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black),
-                          borderRadius: BorderRadius.all(Radius.circular(10)))),
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.only(left: 25, right: 25, top: 10),
-                child: const TextField(
-                  decoration: InputDecoration(
-                      labelText: 'Nhập lại mật khẩu',
-                      labelStyle: TextStyle(color: Colors.grey, fontSize: 15),
-                      border: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black),
-                          borderRadius: BorderRadius.all(Radius.circular(10)))),
-                ),
+              const SizedBox(
+                height: 50,
               ),
               Padding(
-                padding: const EdgeInsets.only(left: 60, top: 20),
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: RichText(
-                    text: TextSpan(
-                        text: "Tôi đồng ý với các ",
-                        style: TextStyle(fontSize: 17, color: Colors.black),
-                        children: [
+                padding: const EdgeInsets.all(10),
+                child: SizedBox(
+                  height: 50,
+                  width: 300,
+                  child: Container(
+                    decoration: const BoxDecoration(
+                        color: Colors.green,
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                    child: Center(
+                      child: RichText(
+                        text: TextSpan(children: [
                           TextSpan(
                             recognizer: TapGestureRecognizer()
                               ..onTap = (() {
                                 Navigator.push(
                                     context,
                                     MaterialPageRoute(
-                                        builder: (context) => LoginScreen()));
+                                        builder: (context) =>
+                                            const SigninScreen()));
                               }),
-                            text: "Điều khoản của dịch vụ",
-                            style: TextStyle(fontSize: 17, color: Colors.blue,decoration: TextDecoration.underline ),
-                          
+                            text: "Đăng ký",
+                            style: const TextStyle(
+                              fontSize: 22,
+                              color: Colors.white,
+                            ),
                           )
                         ]),
+                      ),
+                    ),
                   ),
                 ),
               ),
-              const SizedBox(
-                height: 20,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(25.0),
-                child: Container(
+              SizedBox(
+                  height: 50,
+                  width: 300,
+                  child: Container(
                     decoration: const BoxDecoration(
                         color: Colors.green,
                         borderRadius: BorderRadius.all(Radius.circular(10))),
-                    height: 50,
                     child: const Center(
                       child: Text(
-                        'Gửi mã xác thực',
-                        style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold),
+                        'Đăng nhập',
+                        style: TextStyle(fontSize: 22, color: Colors.white,
                       ),
-                    )),
-              ),
-              const Padding(
-                padding: EdgeInsets.only(left: 25, right: 25, top: 100),
-                child: Text(
-                  'Bạn có thể đăng ký với :',
-                  style: TextStyle(fontSize: 17, color: Colors.black),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 90, right: 90, top: 30),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      width: 50,
-                      height: 50,
-                      padding: const EdgeInsets.all(15),
-                      decoration: const BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(15)),
-                        color: Colors.blue,
-                      ),
-                      child: const FlutterLogo(),
                     ),
-                    Container(
-                      width: 50,
-                      height: 50,
-                      padding: const EdgeInsets.all(15),
-                      decoration: const BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(15)),
-                        color: Colors.blue,
-                      ),
-                      child: const FlutterLogo(),
-                    ),
-                    Container(
-                      width: 50,
-                      height: 50,
-                      padding: const EdgeInsets.all(15),
-                      decoration: const BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(15)),
-                        color: Colors.blue,
-                      ),
-                      child: const FlutterLogo(),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.only(left: 40, right: 25, top: 20),
-                child: RichText(
-                  text: TextSpan(
-                      text: "Bạn đã có tài khoản VietPlant ? ",
-                      style: TextStyle(fontSize: 17, color: Colors.black),
-                      children: [
-                        TextSpan(
-                            recognizer: TapGestureRecognizer()
-                              ..onTap = (() {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => LoginScreen()));
-                              }),
-                            text: "Đăng nhập",
-                            style: TextStyle(fontSize: 17, color: Colors.blue))
-                      ]),
-                ),
-              ),
-            ])),
-      )),
+                  )),
+          )],
+          ),
+        ),
+      ),
     );
   }
 }
